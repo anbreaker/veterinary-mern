add76f90c2931d8677eafc2fc2982d5078453453
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-05-26 16:37:14 +0200

    simple crud completed

--- backend/controllers/patient.controller.js
+++ backend/controllers/patient.controller.js
@@ -9,6 +9,8 @@ export const patientRegisterController = async (req, res) => {
 
     res.json(newPatient);
   } catch (error) {
+    console.log(error);
+
     res.status(500).json({
       message: 'Error registering patient',
       error,
@@ -18,12 +20,94 @@ export const patientRegisterController = async (req, res) => {
 
 export const getAllPatientsController = async (req, res) => {
   try {
-    const patients = await Patient.find().where('veterinary').equals(req.veterinary._id);
+    const patients = await Patient.find().where('veterinary').equals(req.veterinary);
 
     res.json(patients);
   } catch (error) {
+    console.log(error);
+
     res.status(500).json({
       message: 'Error getting patients',
+      error,
+    });
+  }
+};
+
+export const getPatientById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const patient = await Patient.findById(id);
+
+    if (patient.veterinary._id.toString() !== req.veterinary._id.toString()) {
+      return res.status(401).json({
+        message: 'Not authorized',
+      });
+    }
+
+    res.json({ patient });
+  } catch (error) {
+    console.log(error);
+
+    res.status(404).json({
+      message: 'Patient not found',
+      error,
+    });
+  }
+};
+
+export const updatePatient = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const patient = await Patient.findById(id);
+
+    if (patient.veterinary._id.toString() !== req.veterinary._id.toString()) {
+      return res.status(401).json({
+        message: 'Not authorized',
+      });
+    }
+
+    // Updated patient
+    patient.name = req.body.name || patient.name;
+    patient.owner = req.body.owner || patient.owner;
+    patient.email = req.body.email || patient.email;
+    patient.data = req.body.data || patient.data;
+    patient.symptoms = req.body.symptoms || patient.symptoms;
+
+    const updatedPatient = await patient.save();
+
+    res.json({ msg: 'Patient Updated', updatedPatient });
+  } catch (error) {
+    console.log(error);
+
+    res.status(404).json({
+      message: 'Patient not found',
+      error,
+    });
+  }
+};
+
+export const deletetePatient = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const patient = await Patient.findById(id);
+
+    if (patient.veterinary._id.toString() !== req.veterinary._id.toString()) {
+      return res.status(401).json({
+        message: 'Not authorized',
+      });
+    }
+
+    await patient.deleteOne();
+
+    res.json({ msg: 'Patient Deleted' });
+  } catch (error) {
+    console.log(error);
+
+    res.status(404).json({
+      message: 'Patient not found',
       error,
     });
   }
--- backend/routes/patient.routes.js
+++ backend/routes/patient.routes.js
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import {
   patientRegisterController,
   getAllPatientsController,
+  getPatientById,
+  updatePatient,
+  deletetePatient,
 } from '../controllers/patient.controller.js';
 
 import { checkAuth } from '../middleware/authMiddleware.js';
@@ -12,7 +15,16 @@ const router = Router();
 // /api/patients/register
 router.post('/register', checkAuth, patientRegisterController);
 
-// /api/patients/getAllPatients
+// /api/patient/getAllPatients
 router.get('/getAllPatients', checkAuth, getAllPatientsController);
+
+// /api/patients/:id
+router.get('/getPatient/:id', checkAuth, getPatientById);
+
+// /api/patient/:id
+router.put('/updatePatient/:id', checkAuth, updatePatient);
+
+// /api/patient/:id
+router.delete('/deletePatient/:id', checkAuth, deletetePatient);
 
 export default router;
