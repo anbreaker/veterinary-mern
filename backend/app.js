4d9ca9de946bd314b937f285afac50e654034ac4
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-05-27 13:02:48 +0200

    configuration cors by domain allowed

--- backend/app.js
+++ backend/app.js
@@ -17,8 +17,20 @@ export const app = express();
 
 // middlewares
 
-// comunications with other servers
-app.use(cors());
+// comunications with other servers with configuration
+const allowedDomains = ['http://localhost:3000'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedDomains.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 // Helmet can help protect your app from some well-known web vulnerabilities
 // by setting HTTP headers appropriately.
